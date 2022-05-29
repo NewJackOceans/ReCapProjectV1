409598755c8d35b68900da2579f4c85b95ccae32
burak karacakaya <burakaracakaya@gmail.com>
2022-05-29 14:17:32 +0300

    Ödev mola 1 29.05.2022

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -18,6 +18,14 @@ namespace Business.Constants
         public static string BrandListed = "Markalar listelendi.";
         public static string CarListed = "Arabalar listelendi.";
         public static string ColorListed = "Tüm renklerdeki arabalar listelendi..";
+        public static string UserAdded = "Kullanıcı eklendi.";
+        public static string UserRemoved = "Kullanıcı sistemden silindi."; 
+        public static string UserUpdated = "Kullanıcı bilgileri güncellendi.";
+        public static string UserListed = "Kullanıcılar listelendi.";
+        public static string CustomerAdded = "Müşteri eklendi.";
+        public static string CustomerRemoved = "Müşteri sistemden silindi.";
+        public static string CustomerUpdated = "Müşteri bilgileri güncellendi.";
+        public static string CustomerListed = "Müşteriler listelendi.";
 
 
     }
